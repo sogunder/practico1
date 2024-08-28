eb5b2bd494072e8de4f327f53244f3046e22e7b3
oscar <sogunder@gmail.com>
2024-08-28 10:04:07 -0400

    endpoints

---
+++ practico1/servicios/EndpointTareas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practico1.servicios
+{
+    internal class EndpointTareas
+    {
+    }
+}
